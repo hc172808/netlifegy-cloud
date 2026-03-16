183917d7d7528a2eddabc6151163fd196e468bed
hc172808 <hc172808@gmail.com>
2026-03-16 07:50:40 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+set -e
+
+echo "Installing NETLIFEGY Cloud..."
+
+bash scripts/install-docker.sh
+bash scripts/install-security.sh
+
+echo "Starting containers..."
+
+cd docker
+docker compose up -d
+
+echo "Installation complete."
