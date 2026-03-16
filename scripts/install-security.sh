648e7144fbd6e010d3b2cc948149417d9c725190
hc172808 <hc172808@gmail.com>
2026-03-16 07:54:12 -0400

    Add security setup in install-security.sh
    Install UFW and Fail2Ban for security hardening.

--- scripts/install-security.sh
+++ scripts/install-security.sh
@@ -1,3 +1,11 @@
-scripts/
-install-docker.sh
-install-security.sh
+#!/bin/bash
+
+apt install -y ufw fail2ban
+
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw --force enable
+
+systemctl enable fail2ban
+systemctl start fail2ban
